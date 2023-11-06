84409b772828832ea85d71708c50bb2692b65438
doll <x@y.z>
2023-11-06 19:02:30 +0800

    update

--- src/main/java/com/cnwy/crawler/security/SecurityConfiguration.java
+++ src/main/java/com/cnwy/crawler/security/SecurityConfiguration.java
@@ -22,6 +22,8 @@ public class SecurityConfiguration extends VaadinWebSecurity {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        http.csrf(csrf -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("/api/**"))).authorizeHttpRequests(authorize -> authorize.requestMatchers(new AntPathRequestMatcher("/api/**")).permitAll());
+
         http.authorizeHttpRequests(
                 authorize -> authorize.requestMatchers(new AntPathRequestMatcher("/images/*.png")).permitAll());
 
